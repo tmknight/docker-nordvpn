4002deae76e7978984b807a7f3946e35a6385076
Travis M Knight <548588+tmknight@users.noreply.github.com>
2022-11-13 08:59:53 -0500

    Refactor https://github.com/bubuntux/nordvpn
    Rebased to `ubuntu:22.04`
    Latest native NordVPN client `3.15.0`
    Update to backend scripts

---
+++ nordvpn.dockerfile
@@ -0,0 +1,31 @@
+FROM ubuntu:22.04
+LABEL autoheal=true
+ARG DEBIAN_FRONTEND=noninteractive
+RUN apt-get update -qq \
+  && apt-get install -y -qq \
+  curl \
+  iputils-ping \
+  libc6 \
+  ## only if desired to obtain the private key
+  # wireguard \
+  && curl -s https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb --output /tmp/nordrepo.deb \
+  && apt-get install -y -qq \
+  /tmp/nordrepo.deb \
+  && apt-get update -qq \
+  && apt-get install -y -qq \
+  nordvpn \
+  && apt-get remove -y -qq nordvpn-release \
+  && apt-get autoremove -y -qq \
+  && apt-get clean -y -qq \
+  && rm -rf \
+  /tmp/* \
+  /var/cache/apt/archives/* \
+  /var/lib/apt/lists/* \
+  /var/tmp/* \
+  && mkdir -p /run/nordvpn
+COPY ./nordvpn_start.sh /usr/bin/start
+COPY ./scripts/ /usr/local/bin/
+EXPOSE 8118
+HEALTHCHECK --start-period=10s --timeout=3s \
+  CMD nordvpn status | grep -Ei "Status: Connected" || exit 1
+CMD start
---
+++ nordvpn_start.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+## Set iptables
+## NET_LOCAL is blocked until VPN connected
+00-firewall
+10-inet
+## IPv6 support desired
+netipv6=$(dockerNetworks6)
+if [ -n "$netipv6" ]
+then
+    10-inet6
+fi
+
+## if not using Nordlynx
+shopt -s nocasematch
+if [[ ${TECHNOLOGY} != "NordLynx" ]]
+then
+    20-tun
+fi
+
+## Not sure about wanting to include this
+## Seems prudent as these are non-VPN connections
+## TBD
+# 40-allowlist >/dev/null
+
+## Start nordvpn daemon
+rm /run/nordvpn/* 2>/dev/null
+nordvpnd >/dev/null &
+while [ ! -S /run/nordvpn/nordvpnd.sock ]
+do
+    echo "Waiting for nord daemon to start..."
+    sleep 2s
+done
+
+## Start nordvpn
+nord_config || exit $?
+nord_login || exit $?
+nord_connect || exit $?
+
+## Allow NET_LOCAL traffic
+30-route
+## IPv6 support desired
+if [ -n "$netipv6" ]
+then
+    30-route6 >/dev/null
+fi
+
+## Expose private key with Wireguard
+if [ $(which wg) ]
+then
+    nord_migrate
+fi
+
+## Long run script
+echo "Startup complete"
+nord_watch &
+wait $!
+exit $?
