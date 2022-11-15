43fba2f0f89c7b3aebdb658687b207447b4ffb33
Travis M Knight <548588+tmknight@users.noreply.github.com>
2022-11-15 16:26:07 -0500

    Timestamp to indicate time of last start

--- nordvpn_start.sh
+++ nordvpn_start.sh
@@ -52,10 +52,7 @@ then
 fi
 
 ## Long run script
-echo "###########################"
-echo "Startup complete"
-date
-echo "###########################"
+echo "Startup complete |" $(date "+%F %T")
 nord_watch &
 wait $!
 exit $?
