1dcc5e89fa9d3e5135bc32608db90ba43dc7cb19
tmknight <tmknight88@gmail.com>
2022-12-04 09:25:03 -0500

    Update healthchecks; fail faster, faster recovery

--- nordvpn_healthcheck.sh
+++ nordvpn_healthcheck.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 STAT=$(nordvpn status | grep -Eio --color=never "Status: Connected")
+CHK=$(curl -Is -m 5 -o /dev/null -w "%{http_code}" "${CHECK_CONNECTION_URL:-https://www.google.com}")
 CODE=0
-if [ -z "$STAT" ]
+if [ -z "$STAT"]
 then
     STAT="Status: Disconnected"
     CODE=1
+elif [[ ! $CHK =~ ^[23] ]]
+then
+    STAT="Status: Unstable"
+    CODE=2
 fi
 echo -n $STAT
 exit $CODE
